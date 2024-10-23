7fcb6a7fef954047468f3fdc341eb880a983f76e
David Obasuyi <hondavidefosa@gmail.com>
2024-10-23 17:22:13 +0100

    First commit - Committing all files to the repository

---
+++ Blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<h1>Blog Home</h1>')
+def about(request):
+    return HttpResponse('<h2>About Blog</h2>')
