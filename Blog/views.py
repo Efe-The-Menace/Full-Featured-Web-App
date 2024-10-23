bc4cc0e60ec2537e288f127766e580130b2cbabe
David Obasuyi <hondavidefosa@gmail.com>
2024-10-23 18:37:20 +0100

    Added home and about templates

--- Blog/views.py
+++ Blog/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'Blog/home.html')
 def about(request):
-    return HttpResponse('<h2>About Blog</h2>')
+    return render(request, 'Blog/about.html')
