50d52ba20d2b49881755ebf4f7350a8caea2625b
David Obasuyi <hondavidefosa@gmail.com>
2024-10-24 17:29:13 +0100

    Improved templates

--- Blog/views.py
+++ Blog/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'EFE',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 23, 2024'
+    },
+    {
+        'author': 'David',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 24, 2024'
+    },
+    
+]
+
 def home(request):
-    return render(request, 'Blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'Blog/home.html', context)
+
 def about(request):
-    return render(request, 'Blog/about.html')
+    return render(request, 'Blog/about.html', {'title': 'About'})
