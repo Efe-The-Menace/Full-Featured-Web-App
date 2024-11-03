849b9a612719fd34625a3f9f329b0f7b548bb303
David Obasuyi <hondavidefosa@gmail.com>
2024-11-03 23:23:14 +0100

    registered app to admin

--- Blog/views.py
+++ Blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'EFE',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'September 23, 2024'
-    },
-    {
-        'author': 'David',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'September 24, 2024'
-    },
-    
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'Blog/home.html', context)
 
