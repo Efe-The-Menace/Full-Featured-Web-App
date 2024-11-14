cd1899dbeba169116171536c27f888bfb195a7cd
David Obasuyi <hondavidefosa@gmail.com>
2024-11-14 15:52:18 +0100

    Finished the User Registration Page

--- Django_full_featured_web_app/urls.py
+++ Django_full_featured_web_app/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path,include
 from users import views as user_views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Blog.urls')),
-    path('register/', user_views.register, name='blog-register'),
+    path('register/', user_views.register, name="blog-register"),
+    
 ]
