dae38ef35ebd2494ab7a17cc705a800d6f999140
David Obasuyi <hondavidefosa@gmail.com>
2025-07-28 17:10:45 +0100

    Testing

--- Django_full_featured_web_app/settings.py
+++ Django_full_featured_web_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = os.environ.get("DEBUG", "False").lower() == True
+DEBUG = True
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
